f36746274e3183f442a5d4f4672b0e95880f43e8
chmubarak <chmohamedmubarak@gmail.com>
2022-10-02 18:09:04 +0530

    add new note and delete note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,47 @@
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return <div>
-        <Header />
-        {notes.map(note =>
-            <Note
-                key={note.key}
-                title={note.title}
-                content={note.content}
-            />
-        )}
-        <Footer />
+
+  const [notes, setNotes] = useState([])
+
+  function deleteNote(id) {
+    setNotes(prevValue => {
+      return prevValue.filter((item, index) => {
+        return id !== index
+      })
+    })
+  }
+
+
+  function makeNote(newNote) {
+    setNotes(prevValue => {
+      return [...prevValue, newNote]
+    })
+
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea
+        onAdd={makeNote}
+      />
+      {notes.map((note, index) => {
+        return <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNote}
+        />
+      })}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
